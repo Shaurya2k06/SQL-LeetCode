43fd3af2c9bd6bd49a968a0e7c423cf35c8e3e39
shaurya2k06 <shaurya2k06@gmail.com>
2025-02-21 00:57:34 +0530

    Debugging

--- Basics.sql
+++ Basics.sql
@@ -1,26 +1,29 @@
+-- This table stores employee information
+-- ID: Unique identifier for each employee
+-- NAME: Name of the employee
+-- SALARY: Salary of the employee
 CREATE TABLE EMPLOYEES (
-    ID INT PRIMARY KEY AUTO_INCREMENT,
-    NAME VARCHAR(50),
+    ID INT PRIMARY KEY IDENTITY(1,1),
+    `NAME` VARCHAR(50),
     SALARY INT
 );
 
-INSERT INTO EMPLOYEES (NAME, SALARY) VALUES
+INSERT INTO EMPLOYEES (`NAME`, SALARY) VALUES
 ('Ramesh', 1000),
 ('Santosh', 2000),
 ('Manish', 3000),
 ('Rahul', 4000),
 ('Rohit', 5000);
 
-SELECT * FROM EMPLOYEES;
-WHERE SALARY > 3000 AND NAME LIKE '%R%';
+SELECT * FROM EMPLOYEES
+WHERE SALARY > 3000 AND `NAME` LIKE '%R%';
 
 
 
 UPDATE EMPLOYEES
 SET SALARY = 6000
-WHERE NAME = 'Ramesh';
+WHERE `NAME` = 'Ramesh';
 
 DELETE FROM EMPLOYEES
-WHERE NAME = 'Rohit';
+WHERE `NAME` = 'Rohit';
 
-DROP INDEX IF EXISTS NAME ON EMPLOYEES;
